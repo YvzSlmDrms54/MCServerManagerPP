8be05a44a6f0cb32efcb03b71db3d1e9acc4e8ee
YvzSlmDrms54 <yavuzselimdurmus.jr@gmail.com>
2026-08-08 20:50:17 +0300

    The EASIEST upload for the EASIEST way to self-host!

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace MCServerManagerPP;
+
+using Application = System.Windows.Application;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var tempServer = new ServerManager();
+
+        if (string.IsNullOrEmpty(tempServer.Config.Language))
+        {
+            var langWindow = new LanguageSelectWindow();
+            bool? result = langWindow.ShowDialog();
+
+            if (result == true)
+            {
+                tempServer.Config.Language = langWindow.SelectedLanguage;
+                tempServer.SaveConfig();
+            }
+        }
+
+        Lang.CurrentLanguage = string.IsNullOrEmpty(tempServer.Config.Language) ? "tr" : tempServer.Config.Language;
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
+}
